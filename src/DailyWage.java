691ab0dac64693c40ca97e4948f76e870707f5f3
SravaniSomalaraju <somalarajusravani8@gmail.com>
2025-12-11 12:54:33 +0530

    calculate daily wage

---
+++ src/DailyWage.java
@@ -0,0 +1,11 @@
+public class DailyWage {
+
+    public static void main(String[] args) {
+
+        int WAGE_PER_HOUR = 20;
+        int FULL_DAY_HOUR = 8;
+
+        int dailyWage = WAGE_PER_HOUR * FULL_DAY_HOUR;
+        System.out.println("Daily Employee Wage = " + dailyWage);
+}
+}
